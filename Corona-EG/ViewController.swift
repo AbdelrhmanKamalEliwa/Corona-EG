cf01e66a88bdb813d03e6b354129c0f45f388a96
Abdelrhman Eliwa <abdelrhmankamaleliwa@gmail.com>
2020-05-24 08:06:42 +0200

    Initial Commit

---
+++ Corona-EG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Corona-EG
+//
+//  Created by Abdelrhman Eliwa on 5/24/20.
+//  Copyright © 2020 Abdelrhman Eliwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
